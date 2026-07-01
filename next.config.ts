6c20b325942f82a1e46822094a81807b194fd9e9
umama-gill <umamagill28@gmail.com>
2026-07-01 21:21:20 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com'], // Next.js ko allow karne ke liye
+  },
+};
+
+export default nextConfig;
